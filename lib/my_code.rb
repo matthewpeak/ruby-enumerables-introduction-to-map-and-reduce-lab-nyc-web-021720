ad0acf2eb0a07e9f9d27dc8f30d446352ddcc621
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 19:31:46 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,29 @@
 # My Code here....
+
+def map_to_negativize(arr)
+  return arr.map{|num| num*-1}
+end
+
+def map_to_no_change(arr)
+  return arr.map{|num|num}
+end
+
+def map_to_double(arr)
+  return arr.map{|num|num*2}
+end
+
+def map_to_square(arr)
+  return arr.map{|num| num*num}
+end
+
+def reduce_to_total(arr, starting_point=0 )
+  return arr.reduce(starting_point){|total, num| total+num}
+end
+
+def reduce_to_all_true(arr)
+   return arr.reduce{ |x, y| !!x && !!y } 
+end
+
+def reduce_to_any_true(arr)
+   return arr.reduce{ |x, y| !!x || !!y } 
+end
