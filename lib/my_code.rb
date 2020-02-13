19ac09ede3c05fef643ec454c1842c6c5b03bb24
Matthew Peak <matthewpeak@gmail.com>
2020-02-13 19:10:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -4,3 +4,16 @@ def map_to_negativize(arr)
   return arr.map{|num| num*-1}
 end
 
+def map_to_no_change(arr)
+  return arr.map{|num|}
+end
+
+def map_to_double(arr)
+  return arr.map{|num|*2}
+end
+
+def map_to_square(arr)
+  return arr.map(|num| num*num)
+end
+
+def 
